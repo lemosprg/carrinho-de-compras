491ed7addb92cc0adfd75ff21dd02c8bac962b7d
Pablo Lemos <lemosprg@gmail.com>
2024-02-22 14:56:29 -0300

    Adição de botão Finalizar

--- js/app.js
+++ js/app.js
@@ -1,24 +1,34 @@
 let totalGeral = 0;
 
 function adicionar() {
-    let produto = document.getElementById('produto').value;
-    let nomeProduto = produto.split('-')[0];
-    let valorProduto = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value;
-    let preco = quantidade * valorProduto;
-    let listaProdutos = document.getElementById('lista-produtos');
-    listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>
+  let produto = document.getElementById('produto').value;
+  let quantidade = document.getElementById('quantidade').value;
+
+  // Verificar se a quantidade inserida é válida
+  if (quantidade <= 0 || isNaN(quantidade)) {
+    alert("Insira uma quantidade válida.");
+    return;
+  }
+
+  let nomeProduto = produto.split('-')[0];
+  let valorProduto = produto.split('R$')[1];
+  let preco = quantidade * valorProduto;
+  let listaProdutos = document.getElementById('lista-produtos');
+  listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto},00</span>
   </section>`;
 
   totalGeral = totalGeral + preco;
   let campoTotal = document.getElementById('valor-total');
-  campoTotal.textContent = `R$${totalGeral}`;
+  campoTotal.textContent = `R$${totalGeral},00`;
   //Limpar campo de quantidade
   document.getElementById('quantidade').value = 0
 
 }
 
+function finalizar() {
+  alert(`O toral de sua compra foi de R$${totalGeral},00.`);
+}
 
 function limpar() {
     totalGeral = 0;
